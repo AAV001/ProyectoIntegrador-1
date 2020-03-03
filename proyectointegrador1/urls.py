b314e4e71a2b81ca4fef42d4f32601282041b615
AAV001 <aarangov1@eafit.edu.co>
2020-03-03 17:41:43 -0500

    Avances Taller 1

--- proyectointegrador1/urls.py
+++ proyectointegrador1/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
+from rest_framework import routers
+from temperatura import views
+
+router = routers.DefaultRouter()
+router.register(r'temperatura', views.TemperaturaViewSet)
 
 urlpatterns = [
+    url(r'^', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
